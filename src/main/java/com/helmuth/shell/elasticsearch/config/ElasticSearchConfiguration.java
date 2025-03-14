605266aaa2f283bf807363f23f909c8d1d5cfc45
Helmuth Hernandez <helhejr2@gmail.com>
2025-03-14 16:00:47 +0100

    add Elasticsearch configuration with authentication and Docker image support

--- src/main/java/com/helmuth/shell/elasticsearch/config/ElasticSearchConfiguration.java
+++ src/main/java/com/helmuth/shell/elasticsearch/config/ElasticSearchConfiguration.java
@@ -5,7 +5,11 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +24,21 @@ public class ElasticSearchConfiguration {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.builder(HttpHost.create(properties.getHost())).build();
+        RestClientBuilder builder = RestClient.builder(HttpHost.create(properties.getHost()));
+        
+        if (properties.getUsername() != null && !properties.getUsername().isEmpty()) {
+            BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
+            basicCredentialsProvider.setCredentials(
+                AuthScope.ANY,
+                new UsernamePasswordCredentials(properties.getUsername(), properties.getPassword())
+            );
+            
+            builder.setHttpClientConfigCallback(httpClientBuilder -> 
+                httpClientBuilder.setDefaultCredentialsProvider(basicCredentialsProvider)
+            );
+        }
+        
+        return builder.build();
     }
     @Bean
     public ElasticsearchTransport elasticsearchTransport(RestClient restClient) {
